359e7a299f1cc2ac21f276188d10d5b5149d8f15
IcaroZK <110555320+IcaroZK@users.noreply.github.com>
2022-08-03 21:53:39 -0300

    Update afk-_afk.js

--- plugins/afk-_afk.js
+++ plugins/afk-_afk.js
@@ -2,9 +2,9 @@ export function before(m) {
     let user = global.db.data.users[m.sender]
     if (user.afk > -1) {
         m.reply(`
-  *[❗𝐈𝐍𝐅𝐎❗] 𝙳𝙴𝙹𝙰𝚂𝚃𝙴 𝙳𝙴 𝙴𝚂𝚃𝙰𝚁 𝙸𝙽𝙰𝙲𝚃𝙸𝚅𝙾 (𝙰𝙵𝙺)${user.afkReason ? ' 𝙳𝙴𝙿𝚄𝙴𝚂 𝙳𝙴 𝙴𝚂𝚃𝙰𝚁 𝙸𝙽𝙰𝙲𝚃𝙸𝚅𝙾 (𝙰𝙵𝙺) 𝙿𝙾𝚁 𝙴𝙻 𝙼𝙾𝚃𝙸𝚅𝙾: ' + user.afkReason : ''}*
+  *[❗𝐈𝐍𝐅𝐎❗] VOCÊ PAROU DE FICAR INATIVO (AFK)${user.afkReason ? ' DEPOIS DE FICAR INATIVO (AFK) PELO MOTIVO: ' + user.afkReason : ''}*
   
-  *—◉ 𝚃𝙸𝙴𝙼𝙿𝙾 𝙳𝙴 𝙸𝙽𝙰𝙲𝚃𝙸𝚅𝙸𝙳𝙰𝙳 (𝙰𝙵𝙺): ${(new Date - user.afk).toTimeString()}*
+  *—◉ TEMPO DE INATIVIDADE (AFK): ${(new Date - user.afk).toTimeString()}*
   `.trim())
         user.afk = -1
         user.afkReason = ''
@@ -18,11 +18,11 @@ export function before(m) {
         if (!afkTime || afkTime < 0)
             continue
         let reason = user.afkReason || ''
-        m.reply(`*[❗] 𝙽𝙾 𝙻𝙾 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙴𝚂 [❗]*
+        m.reply(`*[❗] NÃO ROTULE [❗]*
 
-*—◉ 𝙴𝙻 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝚀𝚄𝙴 𝚄𝚂𝚃𝙴𝙳 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙾 𝙴𝚂𝚃𝙰 𝙸𝙽𝙰𝙲𝚃𝙸𝚅𝙾 (𝙰𝙵𝙺)*      
-*—◉ ${reason ? '𝙼𝙾𝚃𝙸𝚅𝙾 𝙳𝙴 𝙸𝙽𝙰𝙲𝚃𝙸𝚅𝙸𝙳𝙰𝙳 (𝙰𝙵𝙺): ' + reason : '𝙼𝙾𝚃𝙸𝚅𝙾 𝙳𝙴 𝙸𝙽𝙰𝙲𝚃𝙸𝚅𝙸𝙳𝙰𝙳 (𝙰𝙵𝙺): _𝙴𝙻 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙽𝙾 𝙴𝚂𝙿𝙴𝙲𝙸𝙵𝙸𝙲𝙾 𝚄𝙽 𝙼𝙾𝚃𝙸𝚅𝙾_'}*
-*—◉ 𝚃𝙸𝙴𝙼𝙿𝙾 𝚃𝚁𝙰𝙽𝚂𝙲𝚄𝚁𝚁𝙸𝙳𝙾 𝙳𝙴 𝙸𝙽𝙰𝙲𝚃𝙸𝚅𝙸𝙳𝙰𝙳 (𝙰𝙵𝙺): ${(new Date - afkTime).toTimeString()}*
+*—◉ O USUÁRIO QUE VOCÊ MARCA ESTÁ INATIVO (AFK)*      
+*—◉ ${reason ? 'MOTIVO DE INATIVIDADE (AFK): ' + reason : 'MOTIVO DE INATIVIDADE (AFK): _O USUÁRIO NÃO ESPECIFICOU O MOTIVO._'}*
+*—◉ TEMPO DE INATIVIDADE DECORRIDO (AFK): ${(new Date - afkTime).toTimeString()}*
   `.trim())
     }
     return true
