7c4057ff4ce9d1f9bf49922cf6bbb8402a1253e2
IcaroZK <110555320+IcaroZK@users.noreply.github.com>
2022-08-04 12:54:42 -0300

    Update buscador-wikipedia.js

--- plugins/buscador-wikipedia.js
+++ plugins/buscador-wikipedia.js
@@ -3,7 +3,7 @@ import fetch from "node-fetch"
 import cheerio from "cheerio"
 async function wikipedia(querry) {
 try {
-const link = await axios.get(`https://es.wikipedia.org/wiki/${querry}`)
+const link = await axios.get(`https://pt.wikipedia.org/wiki/${querry}`)
 const $ = cheerio.load(link.data)
 let judul = $('#firstHeading').text().trim()
 let thumb = $('#mw-content-text').find('div.mw-parser-output > div:nth-child(1) > table > tbody > tr:nth-child(2) > td > a > img').attr('src') || `//i.ibb.co/nzqPBpC/http-error-404-not-found.png`
@@ -25,16 +25,16 @@ status: link.status,
 Pesan: eror}
 return notFond}}
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) throw `${mg}𝙀𝙎𝘾𝙍𝙄𝘽𝘼 𝙇𝘼 𝙋𝘼𝙇𝘼𝘽𝙍𝘼 𝘾𝙇𝘼𝙑𝙀 𝙋𝘼𝙍𝘼 𝘽𝙐𝙎𝘾𝘼𝙍\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} Luna*\n\n𝙏𝙔𝙋𝙀 𝙏𝙃𝙀 𝙆𝙀𝙔𝙒𝙊𝙍𝘿 𝙏𝙊 𝙎𝙀𝘼𝙍𝘾𝙃\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀\n*${usedPrefix + command} Universe*`
+if (!text) throw `${mg}ESCREVA PALAVRA-CHAVE PARA PESQUISAR\nEXEMPLO\n*${usedPrefix + command} Universo*\n\nTYPE THE KEYWORD TO SEARCH\nEXAMPLE\n*${usedPrefix + command} Universe*`
 wikipedia(`${text}`).then(res => {
-let info = `𝙀𝙉𝘾𝙊𝙉𝙏𝙍𝙀 𝙀𝙎𝙏𝙊 | 𝙄 𝙁𝙊𝙐𝙉𝘿 𝙏𝙃𝙄𝙎:\n\n` + res.result.isi
-conn.sendHydrated(m.chat, info, wm, null, ig, '𝙄𝙣𝙨𝙩𝙖𝙜𝙧𝙖𝙢', null, null, [
-['𝙈𝙚𝙣𝙪 𝘽𝙪𝙨𝙦𝙪𝙚𝙙𝙖𝙨 | 𝙎𝙚𝙖𝙧𝙘𝙝𝙚𝙨 🔎', '#buscarmenu'],
-['𝙈𝙚𝙣𝙪 𝘾𝙤𝙢𝙥𝙡𝙚𝙩𝙤 | 𝙁𝙪𝙡𝙡 𝙈𝙚𝙣𝙪 ✨', '.allmenu'],
-['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
+let info = `ENCONTREI ISSO | / FOUND THIS:\n\n` + res.result.isi
+conn.sendHydrated(m.chat, info, wm, null, ig, 'XD', null, null, [
+['MENU PESQUISAS | SEARCHES 🔎', '#buscarmenu'],
+['MENU COMPLETO | FULL MENU ✨', '.allmenu'],
+['VOLTAR AO MENU| BACK TO MENU ☘️', '/menu']
 ], m,)   
   
-}).catch(() => { m.reply(`${fg}𝙉𝙊 𝙎𝙀 𝙀𝙉𝘾𝙊𝙉𝙏𝙍𝙊 𝙇𝙊 𝙌𝙐𝙀 𝘽𝙐𝙎𝘾𝘼. 𝙋𝙍𝙊𝘾𝙐𝙍𝙀 𝙐𝙎𝘼𝙍 𝙐𝙉𝘼 𝙋𝘼𝙇𝘼𝘽𝙍𝘼 𝘾𝙇𝘼𝙑𝙀\n\n𝙉𝙊𝙏 𝙁𝙊𝙐𝙉𝘿 𝙒𝙃𝘼𝙏 𝙔𝙊𝙐 𝘼𝙍𝙀 𝙇𝙊𝙊𝙆𝙄𝙉𝙂 𝙁𝙊𝙍. 𝙏𝙍𝙔 𝙏𝙊 𝙐𝙎𝙀 𝘼 𝙆𝙀𝙔 𝙒𝙊𝙍𝘿`) })}
+}).catch(() => { m.reply(`${fg}NÃO ENCONTREI OQUE BUSCA. TENTE USAR OUTRA PALAVRA-CHAVE\n\nNOT FOUND WHAT YOU ARE LOOKING FOR. TRY TO USE A KEYWORD`) })}
 handler.help = ['wikipedia'].map(v => v + ' <apa>')
 handler.tags = [ 'internet']
 handler.command = /^(wiki|wikipedia)$/i 
