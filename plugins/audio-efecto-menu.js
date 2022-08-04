1e944eac8c4ea10d1cdc549bf14b2e0c135a893e
IcaroZK <110555320+IcaroZK@users.noreply.github.com>
2022-08-03 22:00:22 -0300

    Update audio-efecto-menu.js

--- plugins/audio-efecto-menu.js
+++ plugins/audio-efecto-menu.js
@@ -68,20 +68,19 @@ let username = conn.getName(who)
 //user.registered = false
 
 let menu = `
-💗 *¡HOLA | HI!* ${username}
+💗 *OLÁ! | HI!* ${username}
 ╭━━〔 *${wm}* 〕━━⬣
 ┃🎧🎧🎧🎧🎧🎧🎧🎧🎧
-┃✪ *EXPERIENCIA | EXP ➺ ${exp}*
+┃✪ *EXPERIÊNCIA | EXP ➺ ${exp}*
 ┃✪ *NIVEL | LEVEL ➺ ${level}*
-┃✪ *ROL ➺* ${role}
-┃✪ *GATACOINS ➺ $ ${money}*
+┃✪ *LIGA ➺* ${role}
+┃✪ *MOEDAS ➺ $ ${money}*
 ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-┃✪ *FECHA ➺ ${week}, ${date}*
-┃✪ *USUARIOS | USERS ➺ ${Object.keys(global.db.data.users).length}* 
+┃✪ *DATA ➺ ${week}, ${date}* 
 ┃🎧🎧🎧🎧🎧🎧🎧🎧🎧
 ╰━━━━━━〔 𓃠 *${vs}* 〕━━━━━━⬣
 
-╭━〔 𝙈𝙊𝘿𝙄𝙁𝙄𝘾𝘼𝙍 𝘼𝙐𝘿𝙄𝙊 〕━⬣
+╭━〔 MODIFICAR ÁUDIO 〕━⬣
 ┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
 ┃🧰 _${usedPrefix}bass_
 ┃🧰 _${usedPrefix}blown_
@@ -96,10 +95,10 @@ let menu = `
 ┃🧰 _${usedPrefix}smooth_
 ┃🧰 _${usedPrefix}tupai_
 ╰━━━━━━━━━━━━━━━━━━━⬣`.trim()
-conn.sendHydrated(m.chat, menu, wm, pp, 'https://github.com/GataNina-Li/GataBot-MD', '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [
-['𝙈𝙚𝙣𝙪́ 𝙘𝙤𝙢𝙥𝙡𝙚𝙩𝙤 | 𝙁𝙪𝙡𝙡 𝙈𝙚𝙣𝙪 💫', '.allmenu'],
-['𝙈𝙚𝙣𝙪 𝙙𝙚𝙨𝙥𝙡𝙚𝙜𝙖𝙗𝙡𝙚 | 𝙈𝙚𝙣𝙪 𝙇𝙞𝙨𝙩 🌟', '/menulista'],
-['𝙈𝙚𝙣𝙪 𝙋𝙧𝙞𝙣𝙘𝙞𝙥𝙖𝙡 | 𝙈𝙖𝙞𝙣 𝙢𝙚𝙣𝙪 ⚡', '#menu']
+conn.sendHydrated(m.chat, menu, wm, pp, 'XD', 'XD', null, null, [
+['MENU COMPLETO | FULL MENU 💫', '.allmenu'],
+['MENU LISTA | MENU LIST 🌟', '/menulista'],
+['MENU PRINCIPAL| MAIN MENU ⚡', '#menu']
 ], m,)
 }
 
